e57b0b5f3abc3287dec7d7a2de34b301d6abda6d
Shiva Kumar <shivakodes@gmail.com>
2023-08-10 20:29:41 +0530

    updated resume

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -7,7 +7,7 @@ interface ButtonProps{
 export default function Button({text}:ButtonProps) {
   return (
     <Link
-      href="https://shrtco.de/bKo9gM"
+      href="https://shrtco.de/X4899P"
       target={"_blank"}
       rel="noreferrer"
       className="font-mulish w-fit px-4 tablet:px-5 rounded-full text-sm md:text-base py-1 tablet:text-lg text-white border-2 border-lime border-double hover:bg-lime hover:text-black hover:font-semibold transition-all duration-200 ease-in-out text-center"
